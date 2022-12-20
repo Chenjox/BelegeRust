10fb85f8374d4e24c16266318b4e9bd891f65048
Chenjox <chenjox.diepelt@gmx.de>
2022-12-21 00:47:49 +0100

    hinzufügen des graphen

--- Traglast/src/main.rs
+++ Traglast/src/main.rs
@@ -1,4 +1,49 @@
 
+#[derive(Debug)]
+struct Point {
+    num: usize,
+    name: String,
+    x: f64,
+    y: f64
+}
+
 fn main() {
     println!("Test");
+    
+    let mut v = Vec::new();
+
+    // Knoten (noch nicht in Koordinaten die ich brauch)
+    for i in 0..7 {
+        v.push(
+            Point {
+                num: i+1,
+                name: format!("A{}",i+1),
+                x: (3.0 * i as f64),
+                y: 7.0
+            }
+        );
+    }
+    for i in 0..7 {
+        v.push(
+            Point {
+                num: i+8,
+                name: format!("A{}",i+1),
+                x: (3.0 * i as f64),
+                y: 3.0
+            }
+        );
+    }
+    for i in 0..4 {
+        v.push(
+            Point {
+                num: i+15,
+                name: format!("B{}",i+1),
+                x: (3.0 * i as f64),
+                y: 0.0
+            }
+        );
+    }
+    // Kanten 
+    
+    println!("{:?}",v);
 }
