1817b8955228e9272b10b689e9f0b875f43a2a5f
Chenjox <chenjox.diepelt@gmx.de>
2022-12-22 09:25:14 +0100

    hinzufügen von schwingungstilgern

--- VonMises/src/main.rs
+++ VonMises/src/main.rs
@@ -4,6 +4,10 @@ use nalgebra::{SMatrix, SVector};
 type Matrix2x2 = SMatrix<f64, 2, 2>;
 type Vector2 = SVector<f64, 2>;
 
+
+type Matrix4x4 = SMatrix<f64, 4, 4>;
+type Vector4 = SVector<f64, 4>;
+
 // Berechnen des ersten Eigenwerts (also der kleinste Eigenwert)
 fn get_first_eigenvalue(matA: Matrix2x2, matB: Matrix2x2, start: Vector2) -> f64 {
     let mut v_i = start;
@@ -32,9 +36,42 @@ fn get_first_eigenvalue(matA: Matrix2x2, matB: Matrix2x2, start: Vector2) -> f64
     return 0.0;
 }
 
-fn main() {
+fn main_alt() {
     let A = Matrix2x2::new(1.0, -2.0, -2.0, 5.0);
     let B = Matrix2x2::new(1.0, 1.0, 1.0, 2.0);
     let start = Vector2::new(1.0, 1.0);
     get_first_eigenvalue(A, B, start);
+}
+
+fn main() {
+    let omega = 12.5664;
+    let dt = 184.02;
+    let mt = 55.22;
+    let kt = 8891.43;
+
+    let mr = 1104.32;
+    let kr = 196056.0;
+
+    let omegadt = omega * dt;
+    let omegamr = omega * omega * mr;
+    let omegamt = omega * omega * mt;
+
+    let mat = Matrix4x4::new(
+        kr + kt + omegamr, -kt, omegadt, -omegadt,
+        -kt, kt-omegamt, -omegadt, omegadt,
+        -omegadt, omegadt, kr + kt + omegamr, -kt,
+        -omegadt, omegadt, -kt, kt-omegamt
+    );
+    let bvec = Vector4::new(
+        10000.0,
+        0.0,
+        0.0,
+        0.0
+    );
+
+    let so = mat.full_piv_lu();
+    let x = so.solve(&bvec);
+
+    println!("{}",x.unwrap());
+    
 }
