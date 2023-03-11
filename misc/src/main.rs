facd0c937049e9689042b0e92d26f42128811afe
Chenjox <chenjox.diepelt@gmx.de>
2023-03-11 15:02:00 +0100

    earthquake

--- misc/src/main.rs
+++ misc/src/main.rs
@@ -1,5 +1,5 @@
 use nalgebra::{Dynamic, OMatrix, SMatrix, SVector};
-use plotters::{coord::types::RangedCoordusize, prelude::*};
+use plotters::{coord::types::RangedCoordusize, prelude::*, style::colors};
 use rand::prelude::*;
 
 type DynMatrix = OMatrix<f64, Dynamic, Dynamic>;
@@ -10,6 +10,18 @@ fn fill_random<R: Rng + ?Sized>(rng: &mut R, mat: &mut DynMatrix) {
             mat[(i, j)] = rng.gen();
         }
     }
+}
+
+fn get_interpolate_color(color1: &RGBColor, color2: &RGBColor, mixing: f64) -> RGBAColor {
+    let rgb1 = color1.rgb();
+    let rgb2 = color2.rgb();
+    let mut rgb3 = (0,0,0);
+
+    rgb3.0 = ((rgb1.0 as f64) *(1.0 - mixing) + (rgb2.0 as f64) *(mixing)) as u8;
+    rgb3.1 = ((rgb1.1 as f64) *(1.0 - mixing) + (rgb2.1 as f64) *(mixing)) as u8;
+    rgb3.2 = ((rgb1.2 as f64) *(1.0 - mixing) + (rgb2.2 as f64) *(mixing)) as u8;
+    let alpha = color1.alpha() *(1.0 - mixing) + color2.alpha() * 1.0;
+    return RGBAColor(rgb3.0, rgb3.1, rgb3.2, alpha)
 }
 
 const OUT_FILE_NAME: &'static str = "misc/earthquake.gif";
@@ -30,18 +42,20 @@ pub fn main() {
 
     fill_random(&mut rng, &mut grid);
 
-    let alpha = 0.09;
+    let alpha = 0.24;
 
     let mut t = 0.0;
     let tstart = 500.0;
     let tdelta = 5.0;
+    let mut avalanche = 0;
+    let mut history: Vec<[f64; 2]> = Vec::new();
     loop {
         if t > tstart + tdelta {
             break;
         }
         let max = grid.max();
         if max >= 1.0 {
-            // Maximum größer 1!
+            // Maximum größer 1!s
             for i in 0..size {
                 for j in 0..size {
                     if grid[(i, j)] >= 1.0 {
@@ -60,6 +74,7 @@ pub fn main() {
                 }
             } // Update Grid
             grid = grid + &update_grid;
+            avalanche = avalanche +1;
             // Reset Matrix
             update_grid.fill(0.0);
         } else {
@@ -69,16 +84,18 @@ pub fn main() {
                 }
             }
             t = t + (1.0 - max);
-            println!("t = {}", t);
+            history.push([t, avalanche as f64]);
+            println!("t = {}, a = {}", t, avalanche);
+            avalanche = 0;
             if t > tstart {
-                root_gif.fill(&RED).unwrap();
+                root_gif.fill(&WHITE).unwrap();
                 for i in 0..size {
                     for j in 0..size {
                         root_gif
                             .draw(&Circle::new(
                                 (i, j),
                                 1,
-                                Into::<ShapeStyle>::into(&BLUE.mix(1.0 - grid[(i, j)])).filled(),
+                                Into::<ShapeStyle>::into(get_interpolate_color(&BLUE,&RED, grid[(i, j)])).filled(),
                             ))
                             .unwrap();
                     }
