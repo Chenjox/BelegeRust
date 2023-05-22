a5afe855e7fd7fc42f1bf37ec88d53137cf7ec6e
Jochen Diepelt <jochen.diepelt@mailbox.tu-dresden.de>
2023-05-22 20:36:44 +0200

    updating sensitivity

--- sensitivity/src/main.rs
+++ sensitivity/src/main.rs
@@ -39,6 +39,16 @@ fn gradient_sensitivity_s1(min: f64, max: f64, order: i32) -> f64 {
     return a * diff_of_squares(min, max, 1).powi(order-2);
 }
 
+fn gradient_sensitivity_sj(min: f64, max: f64, order: i32) -> f64 {
+    let a1 = 160_000.0/7.0 * diff_of_squares(min, max, 7) * diff_of_squares(min, max, 1).powi(2);
+    let a2 = 1.0/5.0 * ( 161_000.0 * diff_of_squares(min, max, 1) - 320_000.0/2.0 * diff_of_squares(min, max, 2) ) * diff_of_squares(min, max, 5) * diff_of_squares(min, max, 1);
+    let a3 = 1.0/4.0 * (- 160_000.0/3.0 * diff_of_squares(min, max, 3) - 1600.0 * diff_of_squares(min, max, 1)) * diff_of_squares(min, max, 4) * diff_of_squares(min, max, 1);
+    let a4 = 1.0/3.0 * ( 40804.0 * diff_of_squares(min, max, 1) - 161_600.0/2.0 * diff_of_squares(min, max, 2) + 160_000.0/3.0 * diff_of_squares(min, max, 3) ) * diff_of_squares(min, max, 3) * diff_of_squares(min, max, 1);
+    let a5 = 1.0/2.0 * ( - 80800.0/3.0 * diff_of_squares(min, max, 3)*diff_of_squares(min, max, 1) - 808.0 * diff_of_squares(min, max, 1).powi(2) + 160_000.0/6.0 * diff_of_squares(min, max, 2) * diff_of_squares(min, max, 3) + 800.0 * diff_of_squares(min, max, 2) * diff_of_squares(min, max, 1) ) * diff_of_squares(min, max, 2);
+    let a6 = (40000.0/5.0 * diff_of_squares(min, max, 5) + 800.0/3.0 * diff_of_squares(min, max, 3) + 4.0 * diff_of_squares(min, max, 1)) * diff_of_squares(min, max, 1).powi(2);
+    return (a1 + a2 + a3 + a4+ a5 +a6) * diff_of_squares(min, max, 1).powi(order-3);
+}
+
 fn gradient_sensitivity_sn(min: f64, max: f64, order: i32) -> f64 {
     return ( 40000.0/3.0 * diff_of_squares(min, max, 3)*diff_of_squares(min, max, 1)
     - 40000.0/3.0 * diff_of_squares(min, max, 2) * diff_of_squares(min, max, 3)
@@ -63,6 +73,9 @@ fn main() {
     let max = 2.046;
     let order = 5;
     let s1 = gradient_sensitivity_s1(min, max, order);
+    let sj = gradient_sensitivity_sj(min, max, order);
     let sn = gradient_sensitivity_sn(min, max, order);
-    println!("{},{}",s1,sn);
+    println!("{},{},{}",s1,sj,sn);
+    let a = s1.max(sj).max(sn);
+    println!("{},{},{}",s1/a,sj/a,sn/a);
 }
