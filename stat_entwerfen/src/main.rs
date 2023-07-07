8e724b52e3de000b7ad56af4017fb0f1b6462983
Chenjox <chenjox.diepelt@gmx.de>
2023-07-07 23:15:13 +0200

    adding schrauben

--- stat_entwerfen/src/main.rs
+++ stat_entwerfen/src/main.rs
@@ -1,12 +1,12 @@
+use std::f64::consts;
+
+mod schrauben;
 
 use plotters::prelude::*;
 
-fn plot_moment_to_file(
-    path: &str,
-    einzellast: f64,
-    pos_last: f64,
-    laenge: f64
-) {
+use crate::schrauben::ISOSchraube;
+
+fn plot_moment_to_file(path: &str, einzellast: f64, pos_last: f64, laenge: f64) {
     let root = BitMapBackend::new(path, (640, 480)).into_drawing_area();
     root.fill(&WHITE).unwrap();
     let mut chart = ChartBuilder::on(&root)
@@ -25,7 +25,13 @@ fn plot_moment_to_file(
                 (
                     x,
                     //querkraft_bei_einzellast(einzellast, pos_last,laenge, x)
-                    LF_zwei_einzellasten_querkraft(x,laenge,pos_last,laenge-pos_last,einzellast),
+                    LF_zwei_einzellasten_querkraft(
+                        x,
+                        laenge,
+                        pos_last,
+                        laenge - pos_last,
+                        einzellast,
+                    ),
                 )
             }),
             &RED,
@@ -45,28 +51,30 @@ fn plot_moment_to_file(
 }
 
 // Auflagerkräfte links und rechts
-fn auflagerkraefte(einzellast: f64, pos_last: f64, laenge: f64) -> [f64;2] {
-    let left_druck = einzellast * ((laenge - pos_last)/laenge).powi(2) * (1.0 + (2.0 * pos_last)/laenge);
-    let right_druck = einzellast * (pos_last/laenge).powi(2) * (1.0 + (2.0 * (laenge -pos_last))/laenge);
-    return [left_druck,right_druck];
+fn auflagerkraefte(einzellast: f64, pos_last: f64, laenge: f64) -> [f64; 2] {
+    let left_druck =
+        einzellast * ((laenge - pos_last) / laenge).powi(2) * (1.0 + (2.0 * pos_last) / laenge);
+    let right_druck =
+        einzellast * (pos_last / laenge).powi(2) * (1.0 + (2.0 * (laenge - pos_last)) / laenge);
+    return [left_druck, right_druck];
 }
 
 // Auflagermomente links und rechts
-fn auflagermomente(einzellast: f64, pos_last: f64, laenge: f64) -> [f64;2] {
-    let left_mom = - einzellast * pos_last * ((laenge-pos_last)/laenge).powi(2);
-    let right_mom = - einzellast * (laenge - pos_last) * (pos_last/laenge).powi(2);
-    return [left_mom,right_mom];
+fn auflagermomente(einzellast: f64, pos_last: f64, laenge: f64) -> [f64; 2] {
+    let left_mom = -einzellast * pos_last * ((laenge - pos_last) / laenge).powi(2);
+    let right_mom = -einzellast * (laenge - pos_last) * (pos_last / laenge).powi(2);
+    return [left_mom, right_mom];
 }
 
 // Auflagerkräfte infolge Linienlast, links und rechts
-fn auflagerkraefte_lineload(linienlast: f64, laenge: f64) -> [f64;2] {
+fn auflagerkraefte_lineload(linienlast: f64, laenge: f64) -> [f64; 2] {
     let auflager = linienlast * laenge / 2.0;
-    return [auflager,auflager];
+    return [auflager, auflager];
 }
 
-fn auflagermomente_lineload(linienlast: f64, laenge: f64) -> [f64;2] {
-    let auflager = - linienlast * laenge*laenge/12.0;
-    return [auflager,auflager];
+fn auflagermomente_lineload(linienlast: f64, laenge: f64) -> [f64; 2] {
+    let auflager = -linienlast * laenge * laenge / 12.0;
+    return [auflager, auflager];
 }
 
 fn moment_bei_einzellast(einzellast: f64, pos_last: f64, laenge: f64, x_wert: f64) -> f64 {
@@ -75,7 +83,7 @@ fn moment_bei_einzellast(einzellast: f64, pos_last: f64, laenge: f64, x_wert: f6
     if x_wert < pos_last {
         return druck[0] * x_wert + mom[0];
     } else {
-        return druck[1] * (laenge- x_wert) + mom[1];
+        return druck[1] * (laenge - x_wert) + mom[1];
     }
 }
 
@@ -89,63 +97,103 @@ fn querkraft_bei_einzellast(einzellast: f64, pos_last: f64, laenge: f64, x_wert:
 }
 
 fn moment_bei_linienlast(linienlast: f64, laenge: f64, x_wert: f64) -> f64 {
-    let mom = auflagermomente_lineload(linienlast,  laenge);
-    let druck = auflagerkraefte_lineload(linienlast,  laenge);
-    return druck[0] * x_wert + mom[0] - linienlast * x_wert * x_wert/2.0;
+    let mom = auflagermomente_lineload(linienlast, laenge);
+    let druck = auflagerkraefte_lineload(linienlast, laenge);
+    return druck[0] * x_wert + mom[0] - linienlast * x_wert * x_wert / 2.0;
 }
 
-fn LF_zwei_einzellasten_moment(x_wert: f64, laenge: f64 ,pos_last_1: f64, pos_last_2: f64, einzellast: f64)-> f64 {
-    return 
-    moment_bei_einzellast(einzellast, pos_last_1, laenge, x_wert)
-    +moment_bei_einzellast(einzellast, pos_last_2, laenge, x_wert);
+fn LF_zwei_einzellasten_moment(
+    x_wert: f64,
+    laenge: f64,
+    pos_last_1: f64,
+    pos_last_2: f64,
+    einzellast: f64,
+) -> f64 {
+    return moment_bei_einzellast(einzellast, pos_last_1, laenge, x_wert)
+        + moment_bei_einzellast(einzellast, pos_last_2, laenge, x_wert);
 }
-fn LF_zwei_einzellasten_querkraft(x_wert: f64, laenge: f64 ,pos_last_1: f64, pos_last_2: f64, einzellast: f64)-> f64 {
-    return 
-    querkraft_bei_einzellast(einzellast, pos_last_1, laenge, x_wert)
-    +querkraft_bei_einzellast(einzellast, pos_last_2, laenge, x_wert);
+fn LF_zwei_einzellasten_querkraft(
+    x_wert: f64,
+    laenge: f64,
+    pos_last_1: f64,
+    pos_last_2: f64,
+    einzellast: f64,
+) -> f64 {
+    return querkraft_bei_einzellast(einzellast, pos_last_1, laenge, x_wert)
+        + querkraft_bei_einzellast(einzellast, pos_last_2, laenge, x_wert);
 }
-
 
 fn nachweis_stirnplattenstoss(
-    querkraft: f64, 
-    moment: f64, 
+    querkraft: f64,
+    moment: f64,
     blechdicke: f64,
     blechlaenge: f64,
-    schraubendurchmesser: f64, 
-    anzahl_schrauben: i32
+    schraubendurchmesser: f64,
+    anzahl_schrauben: i32,
 ) {
-// TODO ! 
+    // TODO !
 }
 
-fn nachweis_biegedrillknicken() {
-    
-}
+fn nachweis_biegedrillknicken() {}
 
 fn main() {
     let laenge = 4.4; // m
     let stuetzenlast = 75.0; // Kn
     let holzlast = 0.192 * 0.7; // kN
-    let einzellast = stuetzenlast + holzlast; // kN 
-    let pos_last = 4.4 - 4.4/2.0 - 0.15 - 0.08; // m
-    let eigengewicht_traeger = 42.5*9.81/1000.0; // In kN/m
+    let einzellast = stuetzenlast + holzlast; // kN
+    let pos_last = 4.4 - 4.4 / 2.0 - 0.15 - 0.08; // m
+    let eigengewicht_traeger = 42.5 * 9.81 / 1000.0; // In kN/m
 
     let emodul = 210000.0;
-    let festigkeit = 235.0 /10.0; // kM/cm^2 
+    let festigkeit = 235.0 / 10.0; // kM/cm^2
 
-    let drittelspunkt = 4.4/3.0;
-    let LF1_moment_stirn=     LF_zwei_einzellasten_moment(drittelspunkt,laenge, pos_last, laenge - pos_last, einzellast);
-    let LF1_moment_mitte =       LF_zwei_einzellasten_moment(laenge/2.0   ,laenge, pos_last, laenge - pos_last, einzellast);
-    let LF1_querkraft_mitte = LF_zwei_einzellasten_querkraft(laenge/2.0   ,laenge, pos_last, laenge - pos_last, einzellast);
-    let LF1_querkraft_stirn = LF_zwei_einzellasten_querkraft(drittelspunkt, laenge, pos_last, laenge - pos_last, einzellast);
+    let drittelspunkt = 4.4 / 3.0;
+    let LF1_moment_stirn = LF_zwei_einzellasten_moment(
+        drittelspunkt,
+        laenge,
+        pos_last,
+        laenge - pos_last,
+        einzellast,
+    );
+    let LF1_moment_mitte = LF_zwei_einzellasten_moment(
+        laenge / 2.0,
+        laenge,
+        pos_last,
+        laenge - pos_last,
+        einzellast,
+    );
+    let LF1_querkraft_mitte = LF_zwei_einzellasten_querkraft(
+        laenge / 2.0,
+        laenge,
+        pos_last,
+        laenge - pos_last,
+        einzellast,
+    );
+    let LF1_querkraft_stirn = LF_zwei_einzellasten_querkraft(
+        drittelspunkt,
+        laenge,
+        pos_last,
+        laenge - pos_last,
+        einzellast,
+    );
     //let LF1_querkraft = querkraft_bei_einzellast(einzellast, pos_last, laenge, drittelspunkt);
 
-    println!("{},{},{},{}",LF1_moment_mitte,LF1_querkraft_mitte,LF1_moment_stirn,LF1_querkraft_stirn);
+    println!(
+        "{},{},{},{}",
+        LF1_moment_mitte, LF1_querkraft_mitte, LF1_moment_stirn, LF1_querkraft_stirn
+    );
 
+    let welast = LF1_moment_mitte * 100.0 / festigkeit * 1.1;
+    println!(
+        "Benötigtes elastisches Widerstandmoment: \n W_el = {}",
+        welast
+    );
 
-    let welast = LF1_moment_mitte*100.0/festigkeit * 1.1;
-    println!("Benötigtes elastisches Widerstandmoment: \n W_el = {}",welast);
+    for i in ISOSchraube::get_schrauben_schneider() {
+        let s = ISOSchraube::new([10, 9], i, false);
 
-
+        println!("{},{}", s.flaeche_schraube(), s.spannungsflaeche_schraube());
+    }
 
     plot_moment_to_file("test.png", einzellast, pos_last, laenge)
 }
